3079b37715adb776f549a421a288bba660c40f1c
Ernie Wang <erniewang@u.northwestern.edu>
2025-08-10 08:35:33 -0500

    slightly fonished not really

--- src/components/pageBase.tsx
+++ src/components/pageBase.tsx
@@ -5,9 +5,10 @@ export default function PageBase({ children }: { children: ReactNode }) {
     const pM = useContext(phoneMode);
     const headerHeight = 65;
 
+    
     return (
         <div
-            className="w-full h-screen bg-black text-white overflow-hidden z-[2]"
+            className="w-full h-screen bg-black text-white overflow-x-hidden overflow-y-scroll z-[2] flex justify-center items-start"
             style={{
                 paddingTop: !pM ? `${headerHeight}px` : 0,
                 paddingBottom: pM ? `${headerHeight}px` : 0
--- src/components/pages/projects/Projects.tsx
+++ src/components/pages/projects/Projects.tsx
@@ -1,18 +1,44 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { phoneMode } from '../../../App';
+import { projectData } from '../../../data/projectDesc';
+import { Project } from '../../../data/projectDesc';
+
+const tS = "text-shadow-strong text-gray-300 text-[clamp(20px,5vw,39px)]";
+const gridS = "rounded-md overflow-hidden bg-gray-200 w-full h-auto mb-4";
 
 export default function Projects() {
-    const pM = useContext(phoneMode);
-
     return (
         <div
             id="projects"
-            className="grid h-full grid-rows-[repeat(3,minmax(200px,full))] sm:grid-cols-2 lg:grid-cols-3 gap-[10px] p-2"
-        >
-            {Array.from({ length: 6 }).map((_, idx) => (
-                <div key={idx} className="bg-gray-800 rounded">
-                </div>
-            ))}
+            className="w-[clamp(300px,90vw,100%)] h-auto flex flex-col items-start gap-2 p-3"
+        >   
+            <p className={tS}>In Progress</p>
+            <div className={gridS}>
+                <div className="w-full h-[100px] bg-gray-400"></div>
+                <div className="w-full h-[100px] bg-gray-500"></div>
+            </div>
+
+            <p className={tS}>AI Focused</p>
+            <div className={gridS}>
+                <div className="w-full h-[100px] bg-gray-400"></div>
+                <div className="w-full h-[100px] bg-gray-500"></div>
+                <div className="w-full h-[100px] bg-gray-600"></div>
+            </div>
+
+            <p className={tS}>Musical Programming</p>
+            <div className={gridS}>
+                <div className="w-full h-[100px] bg-gray-400"></div>
+                <div className="w-full h-[100px] bg-gray-500"></div>
+                <div className="w-full h-[100px] bg-gray-600"></div>
+                <div className="w-full h-[100px] bg-gray-700"></div>
+            </div>
+
+            <p className={tS}>Performance | Composition</p>
+            <div className={gridS}>
+                <div className="w-full h-[100px] bg-gray-400"></div>
+                <div className="w-full h-[100px] bg-gray-500"></div>
+                <div className="w-full h-[100px] bg-gray-600"></div>
+            </div>
         </div>
     );
 }
